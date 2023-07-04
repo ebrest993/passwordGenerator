952f066a0ad4d89cc9d02462757fdc0ea660b40b
Elliott Brest <elliott.brest@gmail.com>
2023-07-04 13:02:17 -0500

    cleaning up code and functions in js

--- Develop/script.js
+++ Develop/script.js
@@ -10,7 +10,6 @@ let passLength = "";
 
 
 // The following calls the function numberChar when the "Generate Password" button is clicked.
-
 generateBtn.addEventListener("click", function() {
   numberChar ();
 })
@@ -20,7 +19,6 @@ generateBtn.addEventListener("click", function() {
   // If the user selects "Cancel" it ends the function.
   // The input is then used to select that many random variables.
   // Those variables are then pushed into the same empty array "passArray".
-
 function numberChar () {
   availSelection = [];
   passArray = [];
@@ -31,22 +29,21 @@ function numberChar () {
     window.alert("PLEASE ENTER A NUMBER BETWEEN 8 AND 128");
     numberChar ();
   } else {
-    selectCase();
+    confirmPass ();
   }
 }
 
 // The following function returns random as many random items as the parameter calls for.
-
 function randNum (input) {
   return Math.floor(Math.random() * input);
 };
 
 // THIS FUNCTION ASKS ABOUT UPPER/LOWERCASE CHARACTERS (DOES NOT RECORD THE ANSWER)
-function selectCase () {
+function confirmPass () {
   let passUpper = window.confirm("Would you like your password to contain uppercase characters?");
     if (passUpper === true) {
       availSelection = availSelection.concat(upLetters);
-    };
+    }
   let passLower = window.confirm("Would you like your password to contain lowercase characters?");
     if (passLower === true) {
       availSelection = availSelection.concat(lowLetters);
@@ -69,35 +66,9 @@ function selectCase () {
   console.log(passArray);
   var textarea = document.getElementById("password");
   textarea.value = passArray.join("");
-}
+} 
 
 
-  // if (passCase === "u" || 
-  //     passCase === "l" || 
-  //     passCase === "b" || 
-  //     passCase === "U" || 
-  //     passCase === "L" || 
-  //     passCase === "B") {
-  //   window.alert("Lookit you go!");
-  // } else {
-  //   window.alert("Please enter U for Uppercase, L for lowercase, or B for both"); 
-  //   selectCase ();
-  // }  
- 
-
-// THIS FUNCTION ASKS ABOUT NUMBERS (DOES NOT RECORD THE ANSWER)
-function confirmNumber () {
-  let passNumber = window.confirm("Would you like your password to contain numbers?");
-  if (passNumber === true) {
-    availSelection.push(numbers);
-    window.alert("Adding numbers to the mix...");
-    confirmSpecial();
-  } else {
-    window.alert("86 numbers. Copy that."); 
-    confirmSpecial();
-  }
- 
-}
 
 // Next I need to:
 //   Separate arrays are created for each of the user selections
@@ -111,37 +82,3 @@ function confirmNumber () {
 //   Functions are made to incorporate any/all possible combinations of the selections
 //   A password is generated
 //   A password is displayed in "#password" text area
-
-// THIS FUNCTION ASKS ABOUT SPECIAL CHARACTERS (DOES NOT RECORD THE ANSWER)
-function confirmSpecial () {
-  let passSpecChar = window.confirm("Would you like your password to contain special characters?");
-  if (passSpecChar === true) {
-    availSelection.push(special);
-    window.alert("WHOAAAAAA");
-  } else {
-    window.alert("Whoa whoa whoa... are you coding right now? I THINK SO");
-  }
-}
-
-function randUpper () {
-  for (var i = 0; i < passCase; i++) {
-    let randCase = randNum(upLetters.length-1);
-    passArray.push(upLetters[randCase]);
-  } console.log(passArray);
-}
-
- function randLower () {
-   for (var i = 0; i < passCase; i++) {
-    let randCase = randNum(lowLetters.length-1);
-    passArray.push(lowLetters[randCase]);
-  } console.log(passArray);
-}
-
-  function randLetters () {
-  for (var i = 0; i < passCase; i++) {
-    let randCase = randNum(letters.length-1);
-    passArray.push(letters[randCase]);
-  } console.log(passArray);
-}
-
-randNum(confirmNumber);
